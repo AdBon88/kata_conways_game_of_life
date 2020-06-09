309ffa7e2131a12502bc7c152d9c549122c684eb
Nhan Dang <nhantdang@gmail.com>
2020-06-09 11:36:26 +1000

    initial commit

---
+++ kata_conways_game_of_life.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace kata_conways_game_of_life.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
