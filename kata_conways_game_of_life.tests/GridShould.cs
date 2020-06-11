cd84faf334407d855141b532bc335f7e110a2a1e
Nhan Dang <nhantdang@gmail.com>
2020-06-11 10:15:48 +1000

    generate grid method complete but not yet tested

--- kata_conways_game_of_life.tests/GridShould.cs
+++ kata_conways_game_of_life.tests/GridShould.cs
@@ -1,7 +1,22 @@
+using System;
+using Xunit;
+
 namespace kata_conways_game_of_life.tests
 {
     public class GridShould
     {
+        [Fact]
+        public void ContainCorrectNumberOfSquares()
+        {
+            var sut = new Grid(3, 3);
+
+            var expectedDisplay = 
+                "[ ][ ][ ]" + Environment.NewLine +
+                "[ ][ ][ ]" + Environment.NewLine +
+                "[ ][ ][ ]";
+            
+            Assert.Equal(expectedDisplay, sut.Display());
+        }
         
     }
 }
--- kata_conways_game_of_life/Grid.cs
+++ kata_conways_game_of_life/Grid.cs
@@ -1,7 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace kata_conways_game_of_life
 {
     public class Grid
     {
+        public Grid(int numberOfRows, int numberOfColumns)
+        {
+            NumberOfRows = numberOfRows;
+            NumberOfColumns = numberOfColumns;
+            _contents = GenerateGrid();
+        }
         
+        public int NumberOfRows { get; }
+        public int NumberOfColumns { get; }
+        private readonly IEnumerable<Location> _contents;
+        
+        private IEnumerable<Location> GenerateGrid()
+        {
+            var gridLocations = new List<Location>();
+            for (int i = 1; i <= NumberOfRows; i++)
+            {
+                for (int j = 1; j <= NumberOfColumns; j++)
+                {
+                    gridLocations.Add(new Location(i, j, new Cell()));
+                }
+            }
+
+            return gridLocations;
+        }
+
+        public string Display()
+        {
+            var rows = _contents.GroupBy(location => location.RowNumber);
+            var gridDisplay = "";
+            
+        }
     }
 }
