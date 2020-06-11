3cf3997700cf28a9e488a1a485a80ffb7d2e0cfa
Nhan Dang <nhantdang@gmail.com>
2020-06-11 14:44:30 +1000

    test passes for top right boundary location neighbours

--- kata_conways_game_of_life.tests/GridShould.cs
+++ kata_conways_game_of_life.tests/GridShould.cs
@@ -154,5 +154,58 @@ namespace kata_conways_game_of_life.tests
 
         }
         
+        [Fact]
+        public void Set8NeighboursToBoundaryTopLeftCornerLocation()
+        {
+            var sut = new Grid(5, 5);
+            var actual = sut.GetNeighboursFor(1, 1);
+
+            
+            var expectedNeighbours = new List<Location>()
+            {
+                new Location(5, 5, new Cell()),
+                new Location(5, 1, new Cell()),
+                new Location(5, 2, new Cell()),
+                new Location(1, 5, new Cell()),
+                new Location(1, 2, new Cell()),
+                new Location(2, 5, new Cell()),
+                new Location(2, 1, new Cell()),
+                new Location(2, 2, new Cell())
+            };
+            
+            for (var i = 0; i < 8; i++)
+            {
+                Assert.Equal( expectedNeighbours.ElementAt(i).RowNumber,actual.ElementAt(i).RowNumber);
+                Assert.Equal( expectedNeighbours.ElementAt(i).ColumnNumber,actual.ElementAt(i).ColumnNumber);
+            }
+
+        }
+        
+        [Fact]
+        public void Set8NeighboursToBoundaryTopRightCornerLocation()
+        {
+            var sut = new Grid(5, 5);
+            var actual = sut.GetNeighboursFor(1, 5);
+
+            
+            var expectedNeighbours = new List<Location>()
+            {
+                new Location(5, 4, new Cell()),
+                new Location(5, 5, new Cell()),
+                new Location(5, 1, new Cell()),
+                new Location(1, 4, new Cell()),
+                new Location(1, 1, new Cell()),
+                new Location(2, 4, new Cell()),
+                new Location(2, 5, new Cell()),
+                new Location(2, 1, new Cell())
+            };
+            
+            for (var i = 0; i < 8; i++)
+            {
+                Assert.Equal( expectedNeighbours.ElementAt(i).RowNumber,actual.ElementAt(i).RowNumber);
+                Assert.Equal( expectedNeighbours.ElementAt(i).ColumnNumber,actual.ElementAt(i).ColumnNumber);
+            }
+
+        }
     }
 }
