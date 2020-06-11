62e46aa4f2ca44df92a8964f82bc0b9a8d986860
Nhan Dang <nhantdang@gmail.com>
2020-06-11 12:26:02 +1000

    test written for left column boundary location

--- kata_conways_game_of_life.tests/GridShould.cs
+++ kata_conways_game_of_life.tests/GridShould.cs
@@ -21,7 +21,7 @@ namespace kata_conways_game_of_life.tests
         }
 
         [Fact]
-        public void Set8SurroundingNeighboursForLocationNotAtBoundary()
+        public void Set8SurroundingNeighboursForNonBoundaryLocation()
         {
             var sut = new Grid(5, 5);
             var actual = sut.GetNeighboursFor(3, 3);
@@ -43,6 +43,36 @@ namespace kata_conways_game_of_life.tests
                 Assert.Equal( expectedNeighbours.ElementAt(i).RowNumber,actual.ElementAt(i).RowNumber);
                 Assert.Equal( expectedNeighbours.ElementAt(i).ColumnNumber,actual.ElementAt(i).ColumnNumber);
             }
+            
+        }
+
+        [Fact]
+        public void Set8NeighboursToBoundaryLeftColumnLocation()
+        {
+            var sut = new Grid(5, 5);
+            var actual = sut.GetNeighboursFor(1, 3);
+
+            
+            var expectedNeighbours = new List<Location>()
+            {
+                new Location(2, 1, new Cell()),
+                new Location(4, 1, new Cell()),
+                new Location(2, 2, new Cell()),
+                new Location(3, 2, new Cell()),
+                new Location(4, 2, new Cell()),
+                new Location(5, 2, new Cell()),
+                new Location(5, 3, new Cell()),
+                new Location(5, 4, new Cell()),
+            };
+            
+            for (var i = 0; i < 8; i++)
+            {
+                Assert.Equal( expectedNeighbours.ElementAt(i).RowNumber,actual.ElementAt(i).RowNumber);
+                Assert.Equal( expectedNeighbours.ElementAt(i).ColumnNumber,actual.ElementAt(i).ColumnNumber);
+            }
+
+            
+            
             
         }
         
