648e910195a2f195efeb2c007663c22fcf4e97ee
Nhan Dang <nhantdang@gmail.com>
2020-06-18 14:23:18 +1000

    readme file added, added figgle font and welcome banner

--- kata_conways_game_of_life/Program.cs
+++ kata_conways_game_of_life/Program.cs
@@ -2,6 +2,7 @@
 using kata_conways_game_of_life.Actions;
 using kata_conways_game_of_life.InputOutput;
 using kata_conways_game_of_life.Models;
+using Figgle;
 
 namespace kata_conways_game_of_life
 {
@@ -9,6 +10,7 @@ namespace kata_conways_game_of_life
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(FiggleFonts.Bubble.Render("Conway's Game of Life"));
             var prompt = new Input();
             var userInputParser = new InputParser(prompt);
             var numberOfRows = userInputParser.ParseGridDimension("rows");
