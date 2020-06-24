1010c95c97bad2ddee888c06a2a74db93070ca1a
Nhan Dang <nhantdang@gmail.com>
2020-06-24 12:23:34 +1000

    added constructor to cell tests

--- kata_conways_game_of_life.tests/CellShould.cs
+++ kata_conways_game_of_life.tests/CellShould.cs
@@ -6,31 +6,33 @@ namespace kata_conways_game_of_life.tests
 {
     public class CellShould
     {
+        public CellShould()
+        {
+            _sut = new Cell();
+        }
+
+        private readonly Cell _sut;
         [Fact]
         public void BeDeadOnCreation()
         {
-            var sut = new Cell();
-            
-            Assert.Equal(State.Dead, sut.State);
+            Assert.Equal(State.Dead, _sut.State);
         }
 
         [Fact]
         public void BeAliveWhenRevived()
         {
-            var sut = new Cell();
-            sut.Revive();
+            _sut.Revive();
             
-            Assert.Equal(State.Alive, sut.State);
+            Assert.Equal(State.Alive, _sut.State);
         }
 
         [Fact]
         public void BeDeadWhenKilled()
         {
-            var sut = new Cell();
-            sut.Revive();
-            sut.Die();
+            _sut.Revive();
+            _sut.Die();
             
-            Assert.Equal(State.Dead, sut.State);
+            Assert.Equal(State.Dead, _sut.State);
         }
         
     }
