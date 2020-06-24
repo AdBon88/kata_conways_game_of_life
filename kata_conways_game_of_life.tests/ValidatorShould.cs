0596044551d7cb1cf7cc5845d38064ee66c38774
Nhan Dang <nhantdang@gmail.com>
2020-06-24 13:47:45 +1000

    Refactored input parser methods to use try-catch, renamed validator methods

--- kata_conways_game_of_life.tests/ValidatorShould.cs
+++ kata_conways_game_of_life.tests/ValidatorShould.cs
@@ -5,18 +5,6 @@ namespace kata_conways_game_of_life.tests
 {
     public class ValidatorShould
     {
-        [Fact]
-        public void IndicateIfStringCoordinatesCanBeParsedToIntegers()
-        {
-            const string coordinates1 = "2, 2";
-            const string coordinates2 = "a, 3";
-            
-            var actual1 = Validator.AreCoordinatesValidNumbers(coordinates1);
-            var actual2 = Validator.AreCoordinatesValidNumbers(coordinates2);
-            
-            Assert.True(actual1);
-            Assert.False(actual2);
-        }
 
         [Fact]
         public void IndicateIfCoordinatesAreWithinGridBoundaries()
@@ -26,8 +14,8 @@ namespace kata_conways_game_of_life.tests
             const int maxGridRow = 3;
             const int maxGridColumn = 3;
 
-            var actual1 = Validator.AreCoordinatesWithinGridBoundaries(coordinates1, maxGridRow, maxGridColumn);
-            var actual2 = Validator.AreCoordinatesWithinGridBoundaries(coordinates2, maxGridRow, maxGridColumn);
+            var actual1 = Validator.IsLocationInGrid(coordinates1, maxGridRow, maxGridColumn);
+            var actual2 = Validator.IsLocationInGrid(coordinates2, maxGridRow, maxGridColumn);
 
             Assert.True(actual1);
             Assert.False(actual2);
@@ -37,11 +25,11 @@ namespace kata_conways_game_of_life.tests
         [Fact]
         public void RequireMinimumOf5RowsAndColumnsForGrid()
         {
-            const string row = "3";
-            const string column = "6";
+            const int row = 3;
+            const int column = 6;
             
-            Assert.False(Validator.IsGridDimensionValid(row));
-            Assert.True(Validator.IsGridDimensionValid(column));
+            Assert.False(Validator.IsDimensionValid(row));
+            Assert.True(Validator.IsDimensionValid(column));
         }
 
     }
